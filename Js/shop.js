a9f0889e46ce8280b9bceca53ac25c0766c931b1
Agustin Amaya <atomix18@gmail.com>
2022-07-31 08:17:04 -0300

    avances en js

--- Js/shop.js
+++ Js/shop.js
@@ -2,7 +2,7 @@ const carrito=[]
 const listaproductos = [
   {
     id: 1,
-    nombre: 'Camiseta Nike Fc Barcelona 2022/23 Stadium Home',
+    nombre: 'Barcelona 2022/23',
     precio: 12999,
     imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw6bdad884/products/NI_DM1840-452/NI_DM1840-452-1.JPG',
     descripcion:"La Camiseta Nike Fc Barcelona 2022/23 Stadium Home es una representación ideal de tu pasión por uno de los equipos más grandes de todos. Combina detalles de diseño basado en la camiseta que usan los profesionales en el campo dándote un look insuperable. Además te brinda comodidad absoluta gracias a su tecnología Dri-FIT para la absorción de sudor. Algo muy importante al momento de elegir esta prenda, es que está hecha en al menos un 50% de fibras recicladas. Porque sabemos que no solo pensás en elegir tu vestimenta, sino que querés hacerlo bien.",
@@ -30,7 +30,7 @@ const listaproductos = [
   },
   {
     id: 3,
-    nombre: 'River 2022',
+    nombre: 'River Plate 2022',
     precio: 14999,
     imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw33965a81/products/AD_GU9603/AD_GU9603-1.JPG',
     descripcion:"La camiseta adidas River Plate 120 Años es más que un homenaje al Millonario y su vida de grandeza. Su diseño posee la banda roja más ancha que nunca, como una alfombra de gala para las ocasiones más especiales que existen. La tecnología AEROREADY te brinda una sensación de comodidad estés donde estés, mientras que la confección con Primegreen, una serie de materiales reciclados de alta performance, le dan el toque justo. Posee el escudo y una leyenda en la zona de la nuca para que la vistas con orgullo todos tus días.",
@@ -59,7 +59,7 @@ const listaproductos = [
   },
   {
     id: 5,
-    nombre: 'Camiseta Umbro C.A.T. 2022 Titular',
+    nombre: 'Camiseta C.A.T. 2022 ',
     precio: 11949,
     imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw9e95136d/products/UB_U31TU01021-7267/UB_U31TU01021-7267-1.JPG',
     descripcion:"La Camiseta Umbro C.A.T 2022 Titular es una prenda ideal para los que eligen llevar el amor por el celeste y blanco a sus partidos más esforzados o al asado con amigos. El cuello escote en V le da un aire futbolístico moderno ideal para lucir en la cancha. Su escudo original festoneado y el logo Umbro bordado imprimen el toque final a tu pasión.",
@@ -74,7 +74,7 @@ const listaproductos = [
   },
   {
     id: 6,
-    nombre: 'Camiseta Nike Liverpool Fc 2022/23 Stadium Home',
+    nombre: 'Liverpool Fc 2022/23',
     precio: 12999,
     imagen: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw969a2eb2/products/NI_DJ7862-609/NI_DJ7862-609-1.JPG',
     descripcion:"La Camiseta Nike Liverpool FC 2022/23 Stadium Home es una representación ideal de tu pasión por uno de los equipos más grandes de todos. Combina detalles de diseño tipo réplica basado en la camiseta que usan los profesionales en el campo dándote un look insuperable. Además te brinda comodidad absoluta gracias a su tecnología Dri-FIT para la absorción de sudor. Algo muy importante al momento de elegir esta prenda, es que está hecha en un 100% de fibras recicladas. Porque sabemos que no solo pensás en elegir tu vestimenta, sino que querés hacerlo bien.",
