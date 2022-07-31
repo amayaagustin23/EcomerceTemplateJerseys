263a538ad3f31fd54b2720bf9514ebbfa79d1237
Agustin Amaya <atomix18@gmail.com>
2022-07-31 01:39:10 -0300

    avances en js

---
+++ Js/cart.js
@@ -0,0 +1,63 @@
+
+const listCart=JSON.parse(localStorage.getItem('myArray'))
+const cantidad=parseInt(localStorage.getItem('cantidad'))
+let totalCompra = listCart.reduce((acum, elemento)=> acum +=elemento.precio, 0)
+document.getElementById("subtotal").innerHTML=`$${totalCompra}`
+document.getElementById("countProducts").innerHTML=`${cantidad} PRODUCTOS `
+
+console.log(listCart)
+
+const result = [...listCart.reduce( (list, item) => {
+    if (!list.has(item.id)) list.set(item.id, { ...item, count: 0 });
+    list.get(item.id).count++;
+    return list;
+}, new Map).values()];
+
+
+console.log(result)
+let select = document.getElementById('select');
+select.addEventListener('change',
+  function(){
+	let selectedOption = this.options[select.selectedIndex];
+	console.log(selectedOption.value);
+  });
+
+function getCart() {
+  return result.map(
+    item =>
+      `  <hr class="my-4" />
+		<div class="row mb-4 d-flex justify-content-between align-items-center">
+		  <div class="col-md-2 col-lg-2 col-xl-2">
+			<img src=${item.imagen} class="img-fluid rounded-3" />
+		  </div>
+		  <div class="col-md-3 col-lg-3 col-xl-3">
+			<h6 class="text-muted">${item.nombre}</h6>
+			<h6 class="text-black mb-0">Camiseta</h6>
+		  </div>
+		  <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+			<button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+			  <i class="fas fa-minus"></i>
+			</button>
+
+			<input id="form1" min="0" name="quantity" value=${item.count} type="number" class="form-control form-control-sm" />
+
+			<button class="btn btn-link px-2" onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+			  <i class="fas fa-plus"></i>
+			</button>
+		  </div>
+		  <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+			<h6 class="mb-0">$${item.precio}</h6>
+		  </div>
+		  <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+			<a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
+		  </div>
+		</div>`
+  );
+}
+
+
+const cartHTML = document.getElementById('containerCart');
+const cartData = getCart();
+cartData.forEach(element => {
+  cartHTML.innerHTML += element;
+});
---
+++ Js/product.js
@@ -0,0 +1,73 @@
+const list=JSON.parse(localStorage.getItem('productos'))
+const id=parseInt(localStorage.getItem('idProducto'))
+const producto=list.find(item=>item.id===id)
+console.log(producto)
+  
+  
+const productoView=`<section>
+<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
+    aria-label="breadcrumb">
+    <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="shop.html">Tienda</a></li>
+    <li class="breadcrumb-item active" aria-current="page">${producto.nombre}</li>
+    </ol>
+    </nav>
+    <br>
+    <div class="containerSection">
+    <div class="imagenProduct">
+    <img src=${producto.imagen}>
+    </div>
+    <div class="contenedor">
+    <h1>${producto.nombre}</h1>
+    <p>Item No. AD_GU9603</p>
+    <br>
+    <h3 class="precio">$${producto.precio}</h3>
+    <div class="talles">
+    <button class="talle">S</button>
+    <button class="talle">M</button>
+    <button class="talle">L</button>
+    <button class="talle">XL</button>
+    </div>
+    <div>
+    <button id="carrito">Agregar al carrito</button>
+    </div>
+    </div>
+    </div>
+    </section>
+    <section>
+    <div class="accordion mt-4" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Descripción
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="descripcionProduct">
+                        <p>${producto.descripcion}</p>
+                    </div>
+                    <div class="listasDesc">
+                        <ol class="listDesc">
+                            <li><span class="descOp">Género:</span> ${producto.genero}</li>
+                            <li><span class="descOp">Material:</span> Poliéster</li>
+                            <li><span class="descOp">Manga: </span> Corta</li>
+                            <li><span class="descOp">Garantía:</span> Contra defecto de fabricación.</li>
+                            <li><span class="descOp">Marca: </span> ${producto.marca}</li>
+                        </ol>
+                        <ol class="listDesc">
+                            <li><span class="descOp">Cuello:</span> ${producto.cuello}</li>
+                            <li><span class="descOp">Calce:</span> ${producto.calce}</li>
+                            <li><span class="descOp">Liga: </span> ${producto.liga}</li>
+                        </ol>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>`;
+    
+    document.getElementById('containerProduct').innerHTML=productoView
