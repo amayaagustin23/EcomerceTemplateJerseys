55b930f2938b987bdd9626d0a8bfcb76b3c2a0a2
Agustin Amaya <atomix18@gmail.com>
2022-08-03 14:08:30 -0300

    avances en js

---
+++ Js/new.js
@@ -0,0 +1,25 @@
+const list = JSON.parse(localStorage.getItem('news'));
+const id = parseInt(localStorage.getItem('idNew'));
+const news = list.find(item => item.id === id);
+console.log(news);
+
+const newView = `      <div class="titleContainer">
+<img src=${news.imagenes[0]} alt=${news.tituloNew} />
+<div class="categorias">
+<p class="categoria">${news.categorias[0]}</p>
+<p class="categoria">${news.categorias[1]}</p>
+<p class="categoria">${news.categorias[2]}</p>
+</div>
+<h1>${news.tituloNew}</h1>
+</div>
+<section>
+<article>
+  <p>${news.parrafos[0]}</p>
+  <img src=${news.imagenes[1]} alt=${news.tituloNew} />
+  <p>${news.parrafos[1]}</p>
+  <img src=${news.imagenes[2]} alt=${news.tituloNew} />
+  <p>${news.parrafos[2]}</p>
+</article>
+</section>
+`;
+document.getElementById('containerNew').innerHTML = newView;
